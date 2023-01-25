888e49606ea07918a9b1a2a407bfbb8ee8d97f03
eminenuryahya <117831162+eminenuryahya@users.noreply.github.com>
2023-01-25 18:23:12 +0300

    Add files via upload

---
+++ TCP/TCPClient/TCPClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer
+{
+    class Program
+    {
+        // TCP bağlantısının açık kalması için
+        static Socket soket = new Socket
+             (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //mesajlarımızın gideceği port.
+        const int PORT = 52000;
+
+        static void Main(string[] args)
+        {
+            
+            string IP = "";
+
+            Console.Title = "TCP Data Transmit - Client";
+
+            //127.0.0.1
+            Console.Write("Bağlanılacak bilgisayarın IP numarası: ");
+            IP = Console.ReadLine();
+
+            Console.WriteLine(IP + " adresindeki bilgisayara bağlanılıyor.Lütfen Bekleyiniz.");
+
+
+            try
+            {
+                soket.Connect(new IPEndPoint(IPAddress.Parse(IP), PORT));
+                Console.WriteLine("Başarıyla bağlanıldı!");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n (X) -> Bağlanmaya çalışırken hata oluştu: " + ex.Message);
+            }
+
+
+            while (true && soket.Connected)
+            {
+                Console.Write("Mesaj: ");
+
+                //Göndermek istediğimiz mesajı yazıyoruz.
+                string gonder = Console.ReadLine();
+
+                //ağ üzerinden göndermek istediğimiz mesajları bytelara dönüştürdük.
+                soket.Send(Encoding.UTF8.GetBytes(gonder));
+                /// 
+                byte[] bytes = new byte[1024];
+                int bytesRec = soket.Receive(bytes);
+                Console.WriteLine("Serverdan Gelen Mesaj = {0}",
+                    Encoding.UTF8.GetString(bytes, 0, bytesRec));
+                ///
+            }
+
+            Console.WriteLine("Programı sonlandırmak için ENTER tuşuna basın...");
+            Console.Read();
+        }
+    }
+}
+
---
+++ TCP/TCPServer/TCPServer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    class Program
+    {
+        //gelen bağlantı isteği üzerine bağlantının saklanacağı yer.
+        static Socket dinleyiciSoket = new Socket
+             (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        const int PORT = 52000;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "TCP Data Transmit - Server";
+
+            // herhangi bir IP adresinden gelebilecek bağlantı isteklerini alabilmesi için;
+            TcpListener dinle = new TcpListener(IPAddress.Any, PORT);
+            dinle.Start();
+
+            Console.WriteLine("Bağlantı bekleniyor...");
+
+            //IP adresi girildikten sonra bağlantı başlıyor 
+            dinleyiciSoket = dinle.AcceptSocket();
+            Console.WriteLine("Bağlantı sağlandı. ");
+
+            while (true)
+            {
+                try
+                {
+                    //Gelecek mesajı içinde saklayabilmemiz için buffer oluşturarak gelen mesajlar bytelanır. Oluşturulan bufferın (gelenData) boyutu belirlenmeli, uzun mesajlar için daha büyük byte kullanılmalı
+
+                    byte[] gelenData = new byte[256];
+                    dinleyiciSoket.Receive(gelenData);  //Receive metodu ile gelen mesaja okuma işlemi yapılır. 
+
+                    //gelen mesajın Türkçe karakter kullanımı UTF8
+                    //Split metodu ise gelen mesajın bittiği noktaya kadar alınmasını sağlar.Çünkü çoğu zaman mesaj byteların tamamını doldurmaz ve kalan karakteri boşluk görür. Split ile bu engellenir.
+                    string mesaj = (Encoding.UTF8.GetString(gelenData)).Split('\0')[0];
+
+                    Console.WriteLine("Gelen mesaj: " + mesaj);
+
+                    /// Burada Gelen mesajı dinleyip mesajın işlem zamanı sonuna ekleyip konsolda yazma işlemi yapıldı.
+                    mesaj += " İşlem Zamanı :  " + DateTime.Now.ToString();
+                    byte[] msg = Encoding.UTF8.GetBytes(mesaj);
+                    dinleyiciSoket.Send(msg);
+                    ///
+
+                    if (mesaj.ToLower().StartsWith("exit"))
+                    {
+
+                        Console.WriteLine("Bağlantı kapatılıyor.");
+                        dinleyiciSoket.Dispose();
+                        break;
+                    }
+                }
+                catch
+                {
+
+                    Console.WriteLine("Bağlantı kesildi. Çıkış yapılıyor.");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Kapatmak için ENTER'a basın.");
+            Console.Read();
+        }
+    }
+}
